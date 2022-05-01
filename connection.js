9d38f5d6e24864967f53728e4b84a2f354f9ebeb
Jonathan Monroy <jmonroybat@gmail.com>
2022-04-30 20:29:20 -0600

    first version

---
+++ connection.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose.connect(
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@cluster0.g9br5.mongodb.net/chatAppMern?retryWrites=true&w=majority`,
+  () => {
+    console.log("Connected to mongodb");
+  }
+);
